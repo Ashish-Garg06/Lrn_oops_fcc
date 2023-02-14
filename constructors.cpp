c87e00001221401e588ac0bdf812a5bef16a2278
Ashish Garg <ashishgarg5006@gmail.com>
2023-02-14 18:26:32 +0530

    Added suitable comments

--- constructors.cpp
+++ constructors.cpp
@@ -1,44 +1,58 @@
 #include <iostream>
 using namespace std;
 
+// Define a class named "Employee"
 class Employee
 {
 public:
+    // Declare public data members
     string name;
     string company;
     int age;
 
+    // Declare a public member function to introduce the employee
     void IntroduceYourself()
     {
+        // Output the employee's name, company, and age to the console
         cout << "Name : " << name << endl;
         cout << "Company : " << company << endl;
         cout << "Age : " << age << endl;
     }
-    // default constructors
+
+    // Define a default constructor for the Employee class
     Employee()
     {
-        name="NA";
-        company="NA";
-        age=00;
+        // Set default values for the data members
+        name = "NA";
+        company = "NA";
+        age = 00;
     }
-    // Parametrized constructor
+
+    // Define a parameterized constructor for the Employee class
     Employee(string argname, string argcompany, int a)
     {
-        name=argname;
-        company=argcompany;
-        age=a;
+        // Initialize the data members using the values provided as arguments
+        name = argname;
+        company = argcompany;
+        age = a;
     }
 };
 
+// Define the main function
 int main()
 {
-    Employee emmployee1("Ashish","Microsoft",22);
-    
-    emmployee1.IntroduceYourself();
-    
-    Employee employee2("Pragya","OpenAi",27);
+    // Create a new Employee object named "employee1" using the parameterized constructor
+    Employee employee1("Ashish", "Microsoft", 22);
 
+    // Call the "IntroduceYourself" method of the employee1 object
+    employee1.IntroduceYourself();
+    Employee employee2("Pragya", "OpenAi", 27);
     employee2.IntroduceYourself();
 
+    // Create a new Employee object named "employee3" using the default constructor
+    Employee employee3;
+
+    // Call the "IntroduceYourself" method of the employee3 object
+    employee3.IntroduceYourself();
     return 0;
 }
